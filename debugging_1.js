062e3762c48029c0bd251454f16e2030bc285eb5
gergokutu <gergo.kutu@gmail.com>
2020-11-19 15:37:35 +0100

    Equality vs assignment

--- debugging_1.js
+++ debugging_1.js
@@ -9,3 +9,16 @@ console.log(seven + three);
 let myArray = [1, 2, 3];
 let arraySum = myArray.reduce((previous, current) => previous + current);
 console.log(`Sum of array values is: ${arraySum}`);
+
+// Assignment operator instead of equality operator
+let x = 7;
+let y = 9;
+let result;
+
+if (x === y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
