d45ff749c2279a6e2bf13b257e262154ffe54492
gergokutu <gergo.kutu@gmail.com>
2020-11-19 15:34:43 +0100

    Catch mispelling, missing chars

--- debugging_1.js
+++ debugging_1.js
@@ -4,3 +4,8 @@ console.log(typeof seven);
 let three = "3";
 console.log(typeof three);
 console.log(seven + three);
+
+// Catch mispelling and missing brackets...
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) => previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
