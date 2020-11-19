3b1720966018c199d98c3a22ea228c98937cea99
gergokutu <gergo.kutu@gmail.com>
2020-11-19 15:47:49 +0100

    Do not forget reinitialization in case of loops

--- debugging_1.js
+++ debugging_1.js
@@ -37,11 +37,28 @@ console.log(power);
 function countToFive() {
   let firstFive = "12345";
   let len = firstFive.length;
-  // Only change code below this line
+
   for (let i = 0; i < len; i++) {
-    // Only change code above this line
     console.log(firstFive[i]);
   }
 }
 
 countToFive();
+
+// Missing reinitialization
+function zeroArray(m, n) {
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      row.push(0);
+    }
+    newArray.push(row);
+    // next line was the missing reinitialization
+    row = [];
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
